4e7fc37c5b822133233ee6f92263470d386a2ee5
HaripriyaGounalan <haripriyaviveck@gmail.com>
2025-03-14 23:22:34 -0400

    SVM Update
    Handled class imbalance and added scripts to simplify code execution.

--- SVM/ModelPerformance.py
+++ SVM/ModelPerformance.py
@@ -22,29 +22,3 @@ class ModelPerformance:
         plt.title("Confusion Matrix")
         plt.savefig(save_path)
         plt.show()
-
-    def plot_classification_report_radial(self, save_path):
-        y_pred = self.model.predict(self.x_test)
-        report_dict = classification_report(self.y_test, y_pred, output_dict=True, zero_division=1)  # Add zero_division=1 here
-        comments = list(report_dict.keys())[:-3]  # Exclude 'accuracy', 'macro avg', 'weighted avg'
-        metrics = ['precision', 'recall', 'f1-score']
-
-        # Data Preparation
-        data = np.array([[report_dict[comment][metric] for metric in metrics] for comment in comments])
-        data = np.concatenate((data, data[:, :1]), axis=1)  # Close the loop for radial chart
-        labels = metrics + [metrics[0]]  # Close the loop for radial chart
-
-        fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(polar=True))
-        theta = np.linspace(0, 2 * np.pi, len(labels), endpoint=True)
-
-        for i, comment in enumerate(comments):
-            ax.plot(theta, data[i], label=f'Class: {comment}')
-            ax.fill(theta, data[i], alpha=0.25)
-
-        ax.set_xticks(theta)
-        ax.set_xticklabels(labels)
-        ax.set_title(f'{self.model} Classification Metrics (Radial)', fontsize=14)  # Use self.model here
-        ax.legend(loc='upper right', bbox_to_anchor=(1.2, 1.1))
-        plt.tight_layout()
-        plt.savefig(save_path)
-        plt.show()
